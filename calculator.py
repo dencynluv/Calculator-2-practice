7846b82c70609765c6a3ef86f7248b487b4f5ad6
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 16:10:40 -0700

    Added all math functions, added greeting and stripped begin space

--- calculator.py
+++ calculator.py
@@ -9,9 +9,34 @@ from arithmetic import *
 
 
 while True:
-    user_equation = raw_input("What equation would you like to perform? \nPlease enter in prefix notation. For example: + 4 5\n")
-    token = user_equation.split(" ")
+    greeting ="""What equation would you like to perform?
+Please enter prefix notation. For example: + 4 5
+If you would like to quit, Enter 'q'"""
+
+    user_equation = raw_input(greeting)
+    token = user_equation.lstrip()
+    token = token.split(" ")
+    
     if token[0] == "q":
+        print "Thanks for playing! See you next time."
         break
     elif token[0] == "+":
         print add(int(token[1]), int(token[2]))
+    elif token[0] == "-":
+        print subtract(int(token[1]), int(token[2]))
+    elif token[0] == "*":
+        print multiply(int(token[1]), int(token[2]))
+    elif token[0] == "/":
+        print divide(int(token[1]), int(token[2]))
+    elif token[0] == "square":
+        print square(int(token[1]))
+    elif token[0] == "cube":
+        print cube(int(token[1]))
+    elif token[0] == "pow":
+        print power(int(token[1]), int(token[2]))
+    elif token[0] == "mod":
+        print mod(int(token[1]), int(token[2]))
+    else:
+        print "Try again!"
+
+        
