a1f37563b175fef32dd037fef86b4ab05c6a0697
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 17:56:04 -0700

    Improved functionality

--- calculator.py
+++ calculator.py
@@ -7,36 +7,50 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-
-while True:
-    greeting ="""What equation would you like to perform?
+greeting ="""What equation would you like to perform?
 Please enter prefix notation. For example: + 4 5
-If you would like to quit, Enter 'q'"""
+If you would like to quit, Enter 'q'
+"""
+print greeting
 
-    user_equation = raw_input(greeting)
+while True:    
+    user_equation = raw_input(">>>")
     token = user_equation.lstrip()
     token = token.split(" ")
-    
+
     if token[0] == "q":
         print "Thanks for playing! See you next time."
         break
-    elif token[0] == "+":
-        print add(int(token[1]), int(token[2]))
-    elif token[0] == "-":
-        print subtract(int(token[1]), int(token[2]))
-    elif token[0] == "*":
-        print multiply(int(token[1]), int(token[2]))
-    elif token[0] == "/":
-        print divide(int(token[1]), int(token[2]))
-    elif token[0] == "square":
-        print square(int(token[1]))
-    elif token[0] == "cube":
-        print cube(int(token[1]))
-    elif token[0] == "pow":
-        print power(int(token[1]), int(token[2]))
-    elif token[0] == "mod":
-        print mod(int(token[1]), int(token[2]))
+    elif len(token) == 2:
+        num1 = float(token[1])
+        
+        if token[0] == "square":
+            print square(num1)
+        elif token[0] == "cube":
+            print cube(num1)
+        else:
+            print "Try Again."
+        
+    elif len(token) == 3:
+        num1 = float(token[1])
+        num2 = float(token[2])
+        
+        if token[0] == "+":
+            print add(num1, num2)
+        elif token[0] == "-":
+            print subtract(num1, num2)
+        elif token[0] == "*":
+            print multiply(num1, num2)
+        elif token[0] == "/":
+            print divide(num1, num2)
+        elif token[0] == "pow":
+            print power(num1, num2)
+        elif token[0] == "mod":
+            print mod(num1, num2)
+        else:
+            print "Try Again."
     else:
-        print "Try again!"
+        print "Try Again."
+        continue
 
         
